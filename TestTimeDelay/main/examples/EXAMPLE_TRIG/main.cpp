6e5320dcf776f0ae39365a864b88eac2be705303
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-05 19:38:38 +0100

    EXAMPLE_TRIG

--- TestTimeDelay/main/examples/EXAMPLE_TRIG/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TRIG/main.cpp
@@ -1,4 +1,4 @@
-/* EXAMPLE_TOF/main.cpp - Application main entry point */
+/* EXAMPLE_TRIG/main.cpp - Application main entry point */
 
 /*
  * Copyright (c) 2017 Intel Corporation
@@ -16,11 +16,12 @@
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
 
-static const char * const TAG = "EXAMPLE_TON";
+static const char * const TAG = "EXAMPLE_TRIG";
 
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
+#define GPIO_Q2 GPIO_NUM_23
 
 
 
@@ -29,7 +30,7 @@ extern "C" void app_main(void)
 {
 
 
-    ESP_LOGI(TAG, "Initializing EXAMPLE_TOF ...");
+    ESP_LOGI(TAG, "Initializing EXAMPLE_TRIG ...");
 
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
        muxed to GPIO on reset already, but some default to other
@@ -38,26 +39,56 @@ extern "C" void app_main(void)
        functions.)
     */
     gpio_reset_pin(GPIO_Q1);
+    gpio_reset_pin(GPIO_Q2);
     gpio_reset_pin(BUTTON_I1);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
+    gpio_set_level(GPIO_Q2, 0); //set to 0 at Reset.
 
+
+
+    F_TRIG F_TRIG1;
+    R_TRIG R_TRIG1;
 
     TOF TOF1;
     TOF1.PT = 1000;
+    TOF TOF2;
+    TOF2.PT = 1000;
+
 
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das "!" bedeutet "NOT" weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = !gpio_get_level(BUTTON_I1);
 
-        // den I1 an TOF2 uebergeben, und TOF2 aufrufen
-        TOF1(I1);
+        // den I1 an F_TRIG1 uebergeben, und F_TRIG1 aufrufen
+        F_TRIG1(I1);
+
+        //Nachricht ausgeben (optional)
+        if (F_TRIG1.Q) {
+        	ESP_LOGI(TAG, "Falling Edge detected on I1 ...");
+        }
+
+        // den I1 an R_TRIG1 uebergeben, und R_TRIG1 aufrufen
+        R_TRIG1(I1);
+
+        //Nachricht ausgeben (optional)
+        if (R_TRIG1.Q) {
+        	ESP_LOGI(TAG, "Raising Edge detected on I1 ...");
+        }
+
+        // den F_TRIG1 an TOF1 uebergeben, und TOF1 aufrufen
+        TOF1(F_TRIG1.Q);
+
+        // den R_TRIG1 an TOF2 uebergeben, und TOF2 aufrufen
+        TOF2(R_TRIG1.Q);
 
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, TOF1.Q);
+        gpio_set_level(GPIO_Q2, TOF2.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
