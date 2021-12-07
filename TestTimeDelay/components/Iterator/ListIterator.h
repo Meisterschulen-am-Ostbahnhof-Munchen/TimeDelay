e806c08ace592dd3bd1339f8d43f10303af4b7e0
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2021-12-07 09:25:07 +0100

    gitflow-feature-stash: Observed

--- TestTimeDelay/components/Iterator/ListIterator.h
+++ TestTimeDelay/components/Iterator/ListIterator.h
@@ -8,6 +8,8 @@ template <class Item>
 class ListIterator : public Iterator<Item> {
 public:
     ListIterator(const List<Item>* aList);
+    ListIterator(const ListIterator& a);
+    ListIterator& operator=(const ListIterator& other);
     virtual void First();
     virtual void Next();
     virtual bool IsDone() const;
